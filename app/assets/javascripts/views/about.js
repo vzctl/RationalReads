36ff526c858ef520ca187631dcefd47e291b8673
Amit Amin <amitpamin@gmail.com>
2015-02-11 17:27:13 -0800

    Reskin of frontpage and footer

--- app/assets/javascripts/views/about.js
+++ app/assets/javascripts/views/about.js
@@ -3,7 +3,7 @@ RationalReads.Views.About = Backbone.CompositeView.extend({
 
   render: function () {
     this.$el.append(this.template());
-
+    RationalReads.Utils.MoveTop();
     return this
   }
 
